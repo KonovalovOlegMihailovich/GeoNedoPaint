b5812849df31d21f277564de20e2ab7e2adf625f
Олег Коновалов <konovalov.105s1@stud.asu.ru>
2022-05-02 16:23:16 +0700

    Добавьте файлы проекта.

---
+++ WinFormsApp3/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace WinFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1689, 1080);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(1689, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(231, 300);
+            this.panel1.TabIndex = 1;
+            // 
+            // button6
+            // 
+            this.button6.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button6.Location = new System.Drawing.Point(0, 145);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(229, 26);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "НиЧеГо";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button7.Location = new System.Drawing.Point(0, 116);
+            this.button7.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(229, 29);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Трапеция";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.buttons_Click);
+            // 
+            // button5
+            // 
+            this.button5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button5.Location = new System.Drawing.Point(0, 87);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(229, 29);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Ромб";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.buttons_Click);
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button4.Location = new System.Drawing.Point(0, 58);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(229, 29);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Прямоугольник";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.buttons_Click);
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button3.Location = new System.Drawing.Point(0, 29);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(229, 29);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Триугольник";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.buttons_Click);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(229, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Эллипс";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buttons_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button10);
+            this.panel2.Controls.Add(this.button9);
+            this.panel2.Controls.Add(this.button8);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(1689, 935);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(231, 145);
+            this.panel2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(0, 30);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(229, 26);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Не закрашивать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button10
+            // 
+            this.button10.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button10.Enabled = false;
+            this.button10.Location = new System.Drawing.Point(0, 56);
+            this.button10.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(229, 29);
+            this.button10.TabIndex = 2;
+            this.button10.Text = "Заливка";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button9
+            // 
+            this.button9.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button9.Enabled = false;
+            this.button9.Location = new System.Drawing.Point(0, 85);
+            this.button9.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(229, 29);
+            this.button9.TabIndex = 1;
+            this.button9.Text = "Штриховка (верт)";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button8
+            // 
+            this.button8.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button8.Enabled = false;
+            this.button8.Location = new System.Drawing.Point(0, 114);
+            this.button8.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(229, 29);
+            this.button8.TabIndex = 0;
+            this.button8.Text = "Штриховка (гор)";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button12
+            // 
+            this.button12.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button12.Location = new System.Drawing.Point(1689, 300);
+            this.button12.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(231, 29);
+            this.button12.TabIndex = 3;
+            this.button12.Text = "Отчистка";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button13.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button13.Location = new System.Drawing.Point(1689, 329);
+            this.button13.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(231, 30);
+            this.button13.TabIndex = 4;
+            this.button13.Text = "Выход";
+            this.button13.UseVisualStyleBackColor = false;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1920, 1080);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button6;
+    }
+}
---
+++ WinFormsApp3/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<Button, ClickHander> buttons;
+        private Dictionary<ClickHander, Dictionary <Button, method>> methods;
+        private delegate void method();
+        private delegate void ClickHander(MouseEventArgs e);
+        method lm = () => { };
+        ClickHander figure = Zero; // Если ничего не выбрано, то ничего не делается =D
+        private Graphics g;
+        private Pen p = new Pen(Color.Black, 2f);
+        private SolidBrush s = new SolidBrush(Color.AliceBlue); 
+        private Pen p2 = new Pen(Color.Black, 1.5f);
+        private Point[] points;
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e) => points[0] = new Point(e.X, e.Y);
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        { 
+            int minx,maxx,miny,maxy;
+            minx = Math.Min(points[0].X, e.X);
+            miny = Math.Min(points[0].Y, e.Y);
+            maxx = Math.Max(points[0].X, e.X);
+            maxy = Math.Max(points[0].Y, e.Y);
+            points[0] = new Point(minx, miny);
+            points[1] = new Point(minx, maxy);
+            points[2] = new Point(maxx, maxy);
+            points[3] = new Point(maxx, miny);
+            points[4] = points[0];
+            figure(e);
+            lm();
+            points = new Point[points.Length];
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+        }
+
+        private void button13_Click(object sender, EventArgs e) => Application.Exit();
+
+        private void Rectangle(MouseEventArgs e) // Прямоугольник
+        {
+            g.DrawPolygon(p, points);
+        }
+
+        private void Ellipse(MouseEventArgs e) // Эллипс
+        {
+            int width = points[0].len(points[3]);
+            int height = points[0].len(points[1]);
+            Point zero = new Point(Math.Min(points[0].X, points[2].X), Math.Min(points[0].Y,points[2].Y));
+            g.DrawEllipse(p, zero.X, zero.Y, width, height);
+        }
+
+        private void Triangle(MouseEventArgs e) // Триугольник
+        {
+            Point[] P = (Point []) points.Clone();
+            P[0] = points[0].middle(points[3]);
+            P[3] = P[0];
+            P[4] = P[0];
+            g.DrawLines(p, P);
+        }
+
+        private void Rhombus(MouseEventArgs e) // Ромб
+        {
+            Point []k = new Point[5];
+            for (int i = 0; i < 5; i++)
+            {
+                k[i] = points[i % 4].middle(points[(i + 1) % 4]);
+            }
+            points = k;
+            g.DrawLines(p, k);
+        }
+
+        private void Trapezoid(MouseEventArgs e) // Трапецию со своей точки зрения я вижу только равнобокой =)
+        {
+            int l = points[0].len(points[3]) / 4;
+            points[0].X += l;
+            points[3].X -= l;
+            points[4] = points[0];
+            g.DrawLines(p, points);
+        }
+        
+        private static void Zero(MouseEventArgs e) { }
+
+        private void buttons_Click(object sender, EventArgs e) // Действие для нажатия любой кнопки отвечающей за фигуры
+        {
+            lm = () => { };
+            foreach (Button el in methods[figure].Keys)
+                el.Enabled = false;
+            figure = buttons[(Button) sender];
+            foreach (Button el in methods[figure].Keys)
+                el.Enabled = true;
+        }
+
+        private int t = 3;
+
+        private void vhatching_Rectangle()
+        {
+            Point a, b;
+            a = new Point(points[0].X, points[0].Y);
+            b = new Point(points[1].X, points[1].Y - 1);
+            while(a.X <= points[3].X)
+            {
+                g.DrawLine(p2, a, b);
+                a.X += t;
+                b.X += t;
+            }
+        }
+
+        private void hhatching_Rectangle()
+        {
+            Point a, b;
+            a = new Point(points[0].X, points[0].Y);
+            b = new Point(points[3].X, points[3].Y);
+            while (a.Y <= points[2].Y)
+            {
+                g.DrawLine(p2, a, b);
+                a.Y +=t;
+                b.Y += t;
+            }
+        }
+
+        private void fill_Rectangle() => g.FillPolygon(s, points);
+
+        private void fill_Ellipse()
+        {
+            int width = points[0].len(points[3]);
+            int height = points[0].len(points[1]);
+            g.FillEllipse(s, points[0].X, points[0].Y, width, height);
+        }
+        
+        private void fill_lines()
+        {
+            System.Drawing.Drawing2D.GraphicsPath path = new System.Drawing.Drawing2D.GraphicsPath();
+            path.AddLines(points);   
+            g.FillPath(s, path);
+        }
+
+        private void clip_Ellipse()
+        {
+            Region rgn = new Region(new Rectangle(points[0].X, points[0].Y, points[0].len(points[3]) + 1, points[0].len(points[1]) + 1));
+            System.Drawing.Drawing2D.GraphicsPath path = new System.Drawing.Drawing2D.GraphicsPath();
+            int width = points[0].len(points[3]);
+            int height = points[0].len(points[1]);
+            path.AddEllipse(points[0].X, points[0].Y, width, height);
+            rgn.Exclude(path);
+            g.FillRegion(Brushes.White, rgn);
+        }
+
+        private void vhatching_Ellipse()
+        {
+            vhatching_Rectangle();
+            clip_Ellipse();
+        }
+
+        private void hhatching_Ellipse()
+        {
+            hhatching_Rectangle();
+            clip_Ellipse();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            g = pictureBox1.CreateGraphics();
+            points = new Point[5];
+            Rectangle s = Screen.PrimaryScreen.Bounds;
+            Size = new Size(s.Width, s.Height);
+            buttons = new Dictionary<Button, ClickHander>
+            {
+                [button1] = this.Ellipse,
+                [button3] = this.Triangle,
+                [button4] = this.Rectangle,
+                [button5] = this.Rhombus,
+                [button6] = Zero,
+                [button7] = this.Trapezoid
+            };
+            methods = new Dictionary<ClickHander, Dictionary<Button, method>>()
+            {
+                [Ellipse] = new Dictionary<Button, method>()
+                {
+                    [button2] = () => { },
+                    [button8] = hhatching_Ellipse,
+                    [button9] = vhatching_Ellipse,
+                    [button10] = fill_Ellipse
+                },
+                [Triangle] = new Dictionary<Button, method>()
+                {
+                    [button2] = () => { },
+                    [button10] = fill_lines
+                },
+                [Rectangle] = new Dictionary<Button, method>()
+                {
+                    [button2] = () => { },
+                    [button8] = hhatching_Rectangle,
+                    [button9] = vhatching_Rectangle,
+                    [button10] = fill_Rectangle
+                },
+                [Rhombus] = new Dictionary<Button, method>()
+                {
+                    [button2] = () => { },
+                    [button10] = fill_lines
+                },
+                [Trapezoid] = new Dictionary<Button, method>()
+                {
+                    [button2] = () => { },
+                    [button10] = fill_lines
+                },
+                [Zero] = new Dictionary<Button, method>() { }
+            };
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e) => lm = methods[figure][(Button)sender];
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            foreach (Button el in methods[Rectangle].Keys)
+                el.Enabled = false;
+            figure = Zero;
+            lm = () => { };
+        }
+    }
+    public static class ext
+    {
+        // функция для нахождения точки посереди отрезка p1-p2
+        public static Point middle(this Point p1, Point p2) => new Point((int)((p1.X + p2.X) / 2), (int)((p1.Y + p2.Y) / 2));
+        // функция для нахождения длины отрезка p1-p2
+        public static int len(this Point p1, Point p2) => (int)(Math.Sqrt((double)(Math.Pow((p1.X - p2.X), 2) + Math.Pow((p1.Y - p2.Y), 2))));
+    }
+}
